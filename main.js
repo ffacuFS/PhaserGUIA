f7d4f1c810d4ebd134cba4de209bf2edcdb2dfda
Denise <deniserey20@hotmail.comm>
2023-04-18 17:00:04 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import HelloWorldScene from "./assets/scenes/HelloWorldScene.js";
+import EscenaVacia from "./assets/scenes/escenavacia.js";
+import Game from "./assets/scenes/Game.js";
 
 // Create a new Phaser config object
 const config = {
@@ -27,7 +29,7 @@ const config = {
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [HelloWorldScene],
+  scene: [Game,HelloWorldScene, EscenaVacia,],
 };
 
 // Create a new Phaser game instance
